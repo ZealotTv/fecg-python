5923dc3c30f148d0aa3e3f08fb2ce6e2a48a8f72
ZealotTv <igor.efremov.368@mail.ru>
2026-02-18 19:34:29 +0300

    Added Dipole parameters dataclass

---
+++ src/modules/generation/subfunctions/datatypes/dmodel_parameters.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, field
+
+import numpy as np
+
+
+@dataclass
+class DmodelParameters:
+    H: np.ndarray
+    VCG: np.ndarray
+    ntype: int = 1
+    SNRfct: np.ndarray = field(
+        default_factory=lambda: np.linspace([-np.pi, np.pi, 250])
+    )
+    stm: np.ndarray
+    rax: float = 0.2
+    ray: float = 0.16
+    raz: float = 0.14
+    rht: float = 0.05
